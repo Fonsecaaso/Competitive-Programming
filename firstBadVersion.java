8db5373761d208b702cfeb5771b8046ddc211356
Mateus Fonseca Piris <mateusfonseca27@hotmail.com>
2023-03-04 13:04:18 -0300

    Create firstBadVersion.java

---
+++ firstBadVersion.java
@@ -0,0 +1,30 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n){        
+        long lo=1,hi=n,mid;
+
+        while(lo<hi){
+            mid = (lo+hi)/2;
+            
+            if(isBadVersion((int)mid)){
+                hi=mid;
+            }else{
+                lo=mid+1;
+            }
+        }
+        
+        return (int)lo;
+        
+    }
+}
+
+/*
+First bad version
+
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product 
+fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. 
+You should minimize the number of calls to the API.
+*/

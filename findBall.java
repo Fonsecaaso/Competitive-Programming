9bbb164fde8f4f9c8621a6559f7ba7d83a35fd7a
Mateus Fonseca Piris <mateusfonseca27@hotmail.com>
2023-03-08 22:07:32 -0300

    Create findBall.java

---
+++ findBall.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        int[] ans = new int[n];
+
+        for(int i=0; i<n; i++){
+            ans[i] = findPath(i, grid);
+        }
+
+        return ans;
+    }
+
+    private int findPath(int index, int[][] grid){
+        int m = grid.length, n = grid[0].length;
+
+        for(int i=0; i<m; i++){
+            if(grid[i][index]==1 && index==n-1) return -1;
+            if(grid[i][index]==-1 && index==0) return -1;
+            if(grid[i][index]==1 && grid[i][index+1]!=1) return -1;
+            if(grid[i][index]==-1 && grid[i][index-1]!=-1) return -1;
+            if(grid[i][index]==1) index++;
+            else index--;
+        }
+
+        return index;
+    }
+}
+
+/*
+1706. Where Will the Ball Fall
+
+You have a 2-D grid of size m x n representing a box, and you have n balls. The box is open on the top and bottom sides.
+
+Each cell in the box has a diagonal board spanning two corners of the cell that can redirect a ball to the right or to the left.
+
+    A board that redirects the ball to the right spans the top-left corner to the bottom-right corner and is represented in the grid as 1.
+    A board that redirects the ball to the left spans the top-right corner to the bottom-left corner and is represented in the grid as -1.
+
+We drop one ball at the top of each column of the box. Each ball can get stuck in the box or fall out of the bottom. A ball gets stuck if it hits a "V" shaped pattern between two boards or if a board redirects the ball into either wall of the box.
+
+Return an array answer of size n where answer[i] is the column that the ball falls out of at the bottom after dropping the ball from the ith column at the top, or -1 if the ball gets stuck in the box.
+*/

2ea3889cf8dde1cc9b322dd8639fb33b951f2f15
Mateus Fonseca Piris <mateusfonseca27@hotmail.com>
2023-03-28 20:05:43 -0300

    Create [Tree] isSameTree.java

---
+++ [Tree] isSameTree.java
@@ -0,0 +1,16 @@
+// 100. Same Tree
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return helper(p,q);
+    }
+
+    boolean helper(TreeNode p, TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+
+        if(p.val!=q.val) return false;
+
+        return helper(p.left,q.left) && helper(p.right,q.right);
+    }
+}
